00bd9c2c15d76ff38343b7d748c3b3f3a225774b
im-ebad <themdebad@gmail.com>
2025-02-09 23:24:18 +0530

    change in sorted

--- recursion/isSorted.cpp
+++ recursion/isSorted.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool isSorted(int index,vector<int>arr)
+
 {
     if(index==arr.size()-1)return true;
     return arr[index]<=arr[index+1]&&isSorted(++index,arr);
 }
+
 int main(){
     vector<int>num{1,2,3,6,5};
     cout<<isSorted(0,num)<<endl;
